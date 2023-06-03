ad4c6dd907373ca395bfab4d0a82d8bbb46d4d8e
Robert Young <david41596@yahoo.com>
2023-06-03 04:07:52 -0400

    start of sign-in route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import { Routes, Route } from "react-router-dom";
-import Navigation from "./routes/navigation/navigation.jsx";
 import Home from "./routes/home/home.jsx";
+import Navigation from "./routes/navigation/navigation.jsx";
+import SignIn from "./routes/sign-in/sign-in.jsx";
+const Shop = () => {
+  return <h1>I am the shop page</h1>;
+};
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+        <Route path="sign-in" element={<SignIn />} />
       </Route>
     </Routes>
   );
