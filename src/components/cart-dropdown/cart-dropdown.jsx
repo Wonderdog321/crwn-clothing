6dc4c808a458e01fc25ab3a75ef67d33e0416e84
Robert Young <david41596@yahoo.com>
2023-06-07 10:28:06 -0400

    Made cart number update, items add to cartdropdown

--- src/components/cart-dropdown/cart-dropdown.jsx
+++ src/components/cart-dropdown/cart-dropdown.jsx
@@ -1,9 +1,17 @@
+import { useContext } from "react";
+import { CartContext } from "../../contexts/cart";
 import Button from "../button/button";
+import CartItem from "../cart-item/cart-item";
 import "./cart-dropdown.scss";
 const CartDropDown = () => {
+  const { cartItems } = useContext(CartContext);
   return (
     <div className="cart-dropdown-container">
-      <div className="cart-items"></div>
+      <div className="cart-items">
+        {cartItems.map((item) => (
+          <CartItem key={item.id} cartItem={item} />
+        ))}
+      </div>
       <Button>GO TO CHECKOUT</Button>
     </div>
   );
